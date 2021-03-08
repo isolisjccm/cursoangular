fc0a4e87524a4929af1b059b76d544a77d8d02c0
Israel Solis Solis <iiss05@jclm.es>
2021-03-08 13:53:27 +0100

    ngIf-ngFor

--- src/app/pages/directivas/directivas.component.ts
+++ src/app/pages/directivas/directivas.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivasComponent implements OnInit {
 
+  titulo = "Centros educativos";
+  colegio = "Colegio Infantes";
+  mostrarColegio = false;
+
+  colegios: string[] = ['Nara', 'Infantes', 'Mayol', 'Sefarad'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  mostrarOcultar(): void  {
+    this.mostrarColegio = !this.mostrarColegio;
   }
 
 }
